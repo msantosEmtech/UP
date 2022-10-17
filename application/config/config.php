4fc1d187f058cb240dffa9c778f198a3fe7db535
Oscar Villicana <oscar.valencia@emtech.digital>
2022-10-17 11:16:09 -0500

    cambios en config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/universia/UP/';
+//$config['base_url'] = 'http://localhost/universia/UP/';
+$config['base_url'] = 'http://localhost:8080/UP/';
 
 /*
 |--------------------------------------------------------------------------
